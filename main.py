d8d77ba9dd96112faf9bf85374670c80e0f98ecf
Gurgen <gurgenarstamyan@icloud.com>
2024-12-18 17:58:34 +0400

    Добавление файла

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1.
+'''
+_num1 = int(input("выведите число 1"))
+_num2 = int(input("выведите число 2"))
+
+print(_num1,_num2)
+'''
+
+'''
+# 2.
+
+_name1 = 'Hello'
+_name2 = 'world'
+
+K_1 = _name1 + " " + _name2
+print(K_1)
+
+K_2 = _name1 * 4
+print(K_2)
+
+print(_name1, end='\n' + _name2)
+
+'''
